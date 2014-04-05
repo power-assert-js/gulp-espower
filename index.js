6c2f4e2847ce81ac0cd1ade2c985f94ad2357b57
Takuto Wada <takuto.wada@gmail.com>
2014-04-05 10:39:02 +0900

    Use espower-source to be DRY and support source-map.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*
+/**
  * gulp-espower - A gulp plugin to apply espower to target files.
  * 
  * https://github.com/twada/gulp-espower
@@ -8,29 +8,18 @@
  *   https://raw.github.com/twada/gulp-espower/master/LICENSE-MIT
  */
 var es = require('event-stream'),
-    espower = require('espower'),
-    esprima = require('esprima'),
-    escodegen = require('escodegen'),
-    merge = require('lodash.merge');
+    espowerSourceToSource = require('espower-source');
 
 module.exports = function (opt) {
     'use strict';
 
     function instrument(file, cb) {
-        var espowerOptions, jsCode, jsAst, modifiedAst;
         if (file.isNull()) {
             cb(null, file); // pass along
         } else if (file.isStream()) {
             cb(new Error('gulp-espower: Streaming not supported'));
         } else {
-            jsCode = file.contents.toString('utf8');
-            jsAst = esprima.parse(jsCode, {tolerant: true, loc: true, range: true, tokens: true});
-            espowerOptions = merge(merge(espower.defaultOptions(), opt), {
-                path: file.path,
-                source: jsCode
-            });
-            modifiedAst = espower(jsAst, espowerOptions);
-            file.contents = new Buffer(escodegen.generate(modifiedAst));
+            file.contents = new Buffer(espowerSourceToSource(file.contents.toString('utf8'), file.path, opt));
             cb(null, file);
         }
     }
