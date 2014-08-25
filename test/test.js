50f72e5d1d90c0631f393a0e7d6beb3d67bea527
Takuto Wada <takuto.wada@gmail.com>
2014-08-25 18:08:42 +0900

    test(gulp-espower): test with or without file.sourceMap cases

--- test/test.js
+++ test/test.js
@@ -11,121 +11,176 @@ var fs = require("fs"),
     espower = require("../");
 
 describe("gulp-espower", function () {
-    
-    it("should produce expected file via buffer", function (done) {
-        var stream = espower(),
+
+    describe("should produce expected file via buffer", function () {
+        var stream, srcFile, expectedFile;
+        beforeEach(function () {
+            stream = espower();
             srcFile = new gutil.File({
                 path: "test/fixtures/example.js",
                 cwd: "test/",
                 base: "test/fixtures",
                 contents: fs.readFileSync("test/fixtures/example.js")
-            }),
+            });
             expectedFile = new gutil.File({
                 path: "test/expected/example.js",
                 cwd: "test/",
                 base: "test/expected",
                 contents: fs.readFileSync("test/expected/example.js")
             });
-        srcFile.sourceMap = {
-            version : 3,
-            file: srcFile.relative,
-            names: [],
-            mappings: '',
-            sources: [srcFile.relative],
-            sourcesContent: [srcFile.contents.toString()]
-        };
-        stream.on("error", function(err) {
-            assert(err);
-            done(err);
         });
-        stream.on("data", function (newFile) {
-            assert(newFile);
-            assert(newFile.contents);
-            assert.equal(newFile.contents.toString() + '\n', expectedFile.contents.toString());
-            assert(newFile.sourceMap);
-            assert.deepEqual(newFile.sourceMap, {
-                "version":3,
-                "sources":["example.js"],
-                "names":["assert","require","truthy","falsy","_expr","_capt","content","filepath","line","equal"],
-                "mappings":"AAAA,IAAIA,MAAA,GAASC,OAAA,CAAQ,cAAR,CAAb,EACIC,MAAA,GAAS,MADb,EAEIC,KAAA,GAAQ,OAFZ;AAGAH,MAAA,CAAOA,MAAA,CAAAI,KAAA,CAAAJ,MAAA,CAAAK,KAAA,CAAAF,KAAA;AAAA,IAAAG,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAP,EAHA;AAIAR,MAAA,CAAOS,KAAP,CAAaT,MAAA,CAAAI,KAAA,CAAAJ,MAAA,CAAAK,KAAA,CAAAH,MAAA;AAAA,IAAAI,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAb,EAAqBR,MAAA,CAAAI,KAAA,CAAAJ,MAAA,CAAAK,KAAA,CAAAF,KAAA;AAAA,IAAAG,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAArB",
-                "file":"example.js",
-                "sourceRoot":"test/fixtures",
-                "sourcesContent":["var assert = require('power-assert'),\n    truthy = 'true',\n    falsy = 'false';\nassert(falsy);\nassert.equal(truthy, falsy);\n"]
+        describe('without upstream sourceMap', function () {
+            it("produce code without sourceMap", function (done) {
+                stream.on("error", function(err) {
+                    assert(err);
+                    done(err);
+                });
+                stream.on("data", function (newFile) {
+                    assert(newFile);
+                    assert(newFile.contents);
+                    assert(expectedFile.contents);
+                    assert.equal(newFile.contents.toString() + '\n', expectedFile.contents.toString());
+                    assert(! newFile.sourceMap);
+                    done();
+                });
+                stream.write(srcFile);
+                stream.end();
             });
-            done();
         });
-        stream.write(srcFile);
-        stream.end();
+        describe('with upstream sourceMap', function () {
+            beforeEach(function () {
+                srcFile.sourceMap = {
+                    version : 3,
+                    file: srcFile.relative,
+                    names: [],
+                    mappings: '',
+                    sources: [srcFile.relative],
+                    sourcesContent: [srcFile.contents.toString()]
+                };
+            });
+            it("push file.sourceMap to downstream", function (done) {
+                stream.on("error", function(err) {
+                    assert(err);
+                    done(err);
+                });
+                stream.on("data", function (newFile) {
+                    assert(newFile);
+                    assert(newFile.contents);
+                    assert.equal(newFile.contents.toString() + '\n', expectedFile.contents.toString());
+                    assert(newFile.sourceMap);
+                    assert.deepEqual(newFile.sourceMap, {
+                        "version":3,
+                        "sources":["example.js"],
+                        "names":["assert","require","truthy","falsy","_expr","_capt","content","filepath","line","equal"],
+                        "mappings":"AAAA,IAAIA,MAAA,GAASC,OAAA,CAAQ,cAAR,CAAb,EACIC,MAAA,GAAS,MADb,EAEIC,KAAA,GAAQ,OAFZ;AAGAH,MAAA,CAAOA,MAAA,CAAAI,KAAA,CAAAJ,MAAA,CAAAK,KAAA,CAAAF,KAAA;AAAA,IAAAG,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAP,EAHA;AAIAR,MAAA,CAAOS,KAAP,CAAaT,MAAA,CAAAI,KAAA,CAAAJ,MAAA,CAAAK,KAAA,CAAAH,MAAA;AAAA,IAAAI,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAb,EAAqBR,MAAA,CAAAI,KAAA,CAAAJ,MAAA,CAAAK,KAAA,CAAAF,KAAA;AAAA,IAAAG,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAArB",
+                        "file":"example.js",
+                        "sourceRoot":"test/fixtures",
+                        "sourcesContent":["var assert = require('power-assert'),\n    truthy = 'true',\n    falsy = 'false';\nassert(falsy);\nassert.equal(truthy, falsy);\n"]
+                    });
+                    done();
+                });
+                stream.write(srcFile);
+                stream.end();
+            });
+        });
     });
+    
 
 
-    it("custom options", function (done) {
-        var stream = espower({
-            patterns: [
-                'refute(actual, [message])',
-                'refute.same(actual, expected, [message])',
-                'refute.isNull(object, [message])'
-            ]
-        }),
+    describe("custom options", function (done) {
+        var stream, srcFile, expectedFile;
+        beforeEach(function () {
+            stream = espower({
+                patterns: [
+                    'refute(actual, [message])',
+                    'refute.same(actual, expected, [message])',
+                    'refute.isNull(object, [message])'
+                ]
+            });
             srcFile = new gutil.File({
                 path: "test/fixtures/customized.js",
                 cwd: "test/",
                 base: "test/fixtures",
                 contents: fs.readFileSync("test/fixtures/customized.js")
-            }),
+            });
             expectedFile = new gutil.File({
                 path: "test/expected/example.js",
                 cwd: "test/",
                 base: "test/expected",
                 contents: fs.readFileSync("test/expected/customized.js")
             });
-        srcFile.sourceMap = {
-            version : 3,
-            file: srcFile.relative,
-            names: [],
-            mappings: '',
-            sources: [srcFile.relative],
-            sourcesContent: [srcFile.contents.toString()]
-        };
-        stream.on("error", function(err) {
-            assert(err);
-            done(err);
         });
-        stream.on("data", function (newFile) {
-            assert(newFile);
-            assert(newFile.contents);
-            assert.equal(newFile.contents.toString() + '\n', expectedFile.contents.toString());
-            assert(newFile.sourceMap);
-            assert.deepEqual(newFile.sourceMap, {
-                version: 3,
-                sources: [ 'customized.js' ],
-                names:
-                [ 'empower',
-                  'require',
-                  'formatter',
-                  'busterAssertions',
-                  'refute',
-                  'targetMethods',
-                  'oneArg',
-                  'twoArgs',
-                  'truthy',
-                  'falsy',
-                  '_expr',
-                  '_capt',
-                  'content',
-                  'filepath',
-                  'line',
-                  'isNull',
-                  'same' ],
-                mappings: 'AAAA,IAAIA,OAAA,GAAUC,OAAA,CAAQ,SAAR,CAAd,EACIC,SAAA,GAAYD,OAAA,CAAQ,wBAAR,CADhB,EAEIE,gBAAA,GAAmBF,OAAA,CAAQ,mBAAR,CAFvB,EAGIG,MAAA,GAASJ,OAAA,CAAQG,gBAAA,CAAiBC,MAAzB,EAAiCF,SAAA,EAAjC,EAA8C;AAAA,QAAEG,aAAA,EAAe;AAAA,YAAEC,MAAA,EAAQ,CAAC,QAAD,CAAV;AAAA,YAAsBC,OAAA,EAAS,CAAC,MAAD,CAA/B;AAAA,SAAjB;AAAA,KAA9C,CAHb,EAIIC,MAAA,GAAS,MAJb,EAKIC,KAAA,GAAQ,OALZ;AAMAL,MAAA,CAAOA,MAAA,CAAAM,KAAA,CAAAN,MAAA,CAAAO,KAAA,CAAAH,MAAA;AAAA,IAAAI,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAP,EANA;AAOAV,MAAA,CAAOW,MAAP,CAAcX,MAAA,CAAAM,KAAA,CAAAN,MAAA,CAAAO,KAAA,CAAAF,KAAA;AAAA,IAAAG,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAd,EAPA;AAQAV,MAAA,CAAOY,IAAP,CAAYZ,MAAA,CAAAM,KAAA,CAAAN,MAAA,CAAAO,KAAA,CAAAH,MAAA;AAAA,IAAAI,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAZ,EAAoBV,MAAA,CAAAM,KAAA,CAAAN,MAAA,CAAAO,KAAA,CAAAF,KAAA;AAAA,IAAAG,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAApB',
-                file: 'customized.js',
-                sourceRoot: 'test/fixtures',
-                sourcesContent: [ 'var empower = require(\'empower\'),\n    formatter = require(\'power-assert-formatter\'),\n    busterAssertions = require("buster-assertions"),\n    refute = empower(busterAssertions.refute, formatter(), { targetMethods: { oneArg: [\'isNull\'], twoArgs: [\'same\'] } }),\n    truthy = \'true\',\n    falsy = \'false\';\nrefute(truthy);\nrefute.isNull(falsy);\nrefute.same(truthy, falsy);\n' ]
+        describe('without upstream sourceMap', function () {
+            it("produce code without sourceMap", function (done) {
+                stream.on("error", function(err) {
+                    assert(err);
+                    done(err);
+                });
+                stream.on("data", function (newFile) {
+                    assert(newFile);
+                    assert(newFile.contents);
+                    assert.equal(newFile.contents.toString() + '\n', expectedFile.contents.toString());
+                    assert(! newFile.sourceMap);
+                    done();
+                });
+                stream.write(srcFile);
+                stream.end();
             });
-            done();
         });
-        stream.write(srcFile);
-        stream.end();
+        describe('with upstream sourceMap', function () {
+            beforeEach(function () {
+                srcFile.sourceMap = {
+                    version : 3,
+                    file: srcFile.relative,
+                    names: [],
+                    mappings: '',
+                    sources: [srcFile.relative],
+                    sourcesContent: [srcFile.contents.toString()]
+                };
+            });
+            it("push file.sourceMap to downstream", function (done) {
+                stream.on("error", function(err) {
+                    assert(err);
+                    done(err);
+                });
+                stream.on("data", function (newFile) {
+                    assert(newFile);
+                    assert(newFile.contents);
+                    assert.equal(newFile.contents.toString() + '\n', expectedFile.contents.toString());
+                    assert(newFile.sourceMap);
+                    assert.deepEqual(newFile.sourceMap, {
+                        version: 3,
+                        sources: [ 'customized.js' ],
+                        names: [
+                            'empower',
+                            'require',
+                            'formatter',
+                            'busterAssertions',
+                            'refute',
+                            'targetMethods',
+                            'oneArg',
+                            'twoArgs',
+                            'truthy',
+                            'falsy',
+                            '_expr',
+                            '_capt',
+                            'content',
+                            'filepath',
+                            'line',
+                            'isNull',
+                            'same'
+                        ],
+                        mappings: 'AAAA,IAAIA,OAAA,GAAUC,OAAA,CAAQ,SAAR,CAAd,EACIC,SAAA,GAAYD,OAAA,CAAQ,wBAAR,CADhB,EAEIE,gBAAA,GAAmBF,OAAA,CAAQ,mBAAR,CAFvB,EAGIG,MAAA,GAASJ,OAAA,CAAQG,gBAAA,CAAiBC,MAAzB,EAAiCF,SAAA,EAAjC,EAA8C;AAAA,QAAEG,aAAA,EAAe;AAAA,YAAEC,MAAA,EAAQ,CAAC,QAAD,CAAV;AAAA,YAAsBC,OAAA,EAAS,CAAC,MAAD,CAA/B;AAAA,SAAjB;AAAA,KAA9C,CAHb,EAIIC,MAAA,GAAS,MAJb,EAKIC,KAAA,GAAQ,OALZ;AAMAL,MAAA,CAAOA,MAAA,CAAAM,KAAA,CAAAN,MAAA,CAAAO,KAAA,CAAAH,MAAA;AAAA,IAAAI,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAP,EANA;AAOAV,MAAA,CAAOW,MAAP,CAAcX,MAAA,CAAAM,KAAA,CAAAN,MAAA,CAAAO,KAAA,CAAAF,KAAA;AAAA,IAAAG,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAd,EAPA;AAQAV,MAAA,CAAOY,IAAP,CAAYZ,MAAA,CAAAM,KAAA,CAAAN,MAAA,CAAAO,KAAA,CAAAH,MAAA;AAAA,IAAAI,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAZ,EAAoBV,MAAA,CAAAM,KAAA,CAAAN,MAAA,CAAAO,KAAA,CAAAF,KAAA;AAAA,IAAAG,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAApB',
+                        file: 'customized.js',
+                        sourceRoot: 'test/fixtures',
+                        sourcesContent: [ 'var empower = require(\'empower\'),\n    formatter = require(\'power-assert-formatter\'),\n    busterAssertions = require("buster-assertions"),\n    refute = empower(busterAssertions.refute, formatter(), { targetMethods: { oneArg: [\'isNull\'], twoArgs: [\'same\'] } }),\n    truthy = \'true\',\n    falsy = \'false\';\nrefute(truthy);\nrefute.isNull(falsy);\nrefute.same(truthy, falsy);\n' ]
+                    });
+                    done();
+                });
+                stream.write(srcFile);
+                stream.end();
+            });
+        });
     });
 
 
